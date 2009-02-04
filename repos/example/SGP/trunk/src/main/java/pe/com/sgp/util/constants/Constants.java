45e1931d45f7a543a07a68b72f23e8ac910f8422
cdoliveirar <cdoliveirar@164a9a6e-b8db-11dd-b55b-2da8d274f894>
2009-02-04 03:17:47 +0000

    Add new constant

--- repos/example/SGP/trunk/src/main/java/pe/com/sgp/util/constants/Constants.java
+++ repos/example/SGP/trunk/src/main/java/pe/com/sgp/util/constants/Constants.java
@@ -3,4 +3,6 @@ package pe.com.sgp.util.constants;
 public class Constants {
 
 	public static final String ERROR_ARTICULO_INVALID = "error.articulo.invalid";
+	public static final String ERROR_ARTICULO_ID_INVALID = "error.articulo.id.invalid";
+	
 }
