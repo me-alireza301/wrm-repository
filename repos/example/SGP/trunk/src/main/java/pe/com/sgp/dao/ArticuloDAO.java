66829db558f96eb80028ecf570c85100ce2f3e57
wroquem <wroquem@164a9a6e-b8db-11dd-b55b-2da8d274f894>
2009-02-05 02:02:16 +0000

    add methods for interface and implements

--- repos/example/SGP/trunk/src/main/java/pe/com/sgp/dao/ArticuloDAO.java
+++ repos/example/SGP/trunk/src/main/java/pe/com/sgp/dao/ArticuloDAO.java
@@ -1,6 +1,7 @@
 package pe.com.sgp.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import pe.com.sgp.domain.Articulo;
 import pe.com.sgp.util.dao.common.GenericDao;
@@ -8,6 +9,8 @@ import pe.com.sgp.util.dao.common.GenericDao;
 public interface ArticuloDAO extends GenericDao{
 	
 	public Articulo findArticuloById(final Serializable id);
+	
+	public List<Articulo> findArticulo();
 	
 	public void saveArticulo(Articulo articulo);
 	
--- repos/example/SGP/trunk/src/main/java/pe/com/sgp/dao/ArticuloDAOImpl.java
+++ repos/example/SGP/trunk/src/main/java/pe/com/sgp/dao/ArticuloDAOImpl.java
@@ -1,6 +1,7 @@
 package pe.com.sgp.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import pe.com.sgp.domain.Articulo;
 import pe.com.sgp.util.constants.Constants;
@@ -11,12 +12,17 @@ public class ArticuloDAOImpl extends GenericDaoHibernate implements ArticuloDAO{
 	public Articulo findArticuloById(Serializable id) {
 		return (id == null ? null : findById(Articulo.class, id));
 	}
+	
+	public List<Articulo> findArticulo() {
+		List<Articulo> list = find(Articulo.class, "from Articulo");
+		return list;
+	}
 
 	public void saveArticulo(Articulo articulo) {
 		if (articulo == null) {
-			throw new RuntimeException(Constants.ERROR_ARTICULO_INVALID);
+			throw new RuntimeException(Constants.ERROR_ARTICULO_ID_INVALID);
 		}
 		super.save(articulo);
 	}
-	
+
 }
