8bdaaa449f85be1da27226ab2d118e523684666e
wroquem <wroquem@164a9a6e-b8db-11dd-b55b-2da8d274f894>
2009-02-11 04:45:31 +0000

    execute ClienteService

--- repos/example/SGP/trunk/src/main/java/pe/com/sgp/run/RunTest.java
+++ repos/example/SGP/trunk/src/main/java/pe/com/sgp/run/RunTest.java
@@ -1,15 +1,12 @@
 package pe.com.sgp.run;
 
-import java.util.Iterator;
-import java.util.List;
-
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
-import pe.com.sgp.domain.Articulo;
-import pe.com.sgp.service.ArticuloService;
+import pe.com.sgp.domain.Cliente;
+import pe.com.sgp.service.ClienteService;
 
 public class RunTest {
 
@@ -20,12 +17,12 @@ public class RunTest {
 		Resource resource = new FileSystemResource(pathFile);
 		BeanFactory factory = new XmlBeanFactory(resource);
 
-		System.out.println("Creando Entity");
+/*		System.out.println("Creando Entity");
 		Articulo articulo = new Articulo();
 		articulo.setDescripcion("Monitores Plasma");
 		articulo.setPrecio(160);
 		System.out.println("obteniendo servicios");
-		ArticuloService articuloService = (ArticuloService) factory.getBean("bizTxArticuloService");
+		ArticuloService articuloService = (ArticuloService) factory.getBean("articuloService");
 		System.out.println("ejecutando servicios");
 		articuloService.saveArticulo(articulo);
 
@@ -35,6 +32,22 @@ public class RunTest {
 			Articulo obj = iterator.next();
 			System.out.println("Articulo = " + obj.getDescripcion());
 		}
+		System.out.println("Fin de Pruebas");*/
+	
+		System.out.println("Creando Entity");
+		Cliente cliente = new Cliente();
+		cliente.setRazonSocial("Adolfo Software Inc");
+		cliente.setRuc("10408696688");
+		cliente.setTelefono("3625844");
+		cliente.setDireccion("lima");
+		cliente.setRepresentante("Adolfo");
+		cliente.setDni("11111111");
+		
+		System.out.println("obteniendo servicios de Spring");
+		ClienteService clienteService =  (ClienteService) factory.getBean("clienteService");
+		
+		System.out.println("Ejecutando servicios de Spring");
+		clienteService.saveCliente(cliente);
 		System.out.println("Fin de Pruebas");
 	}
 
