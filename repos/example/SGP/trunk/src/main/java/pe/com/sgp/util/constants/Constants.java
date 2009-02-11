724c7aa1c62b80b7e25d3b69e6f09af227ca0861
wroquem <wroquem@164a9a6e-b8db-11dd-b55b-2da8d274f894>
2009-02-11 04:44:58 +0000

    Add new Cliente constants

--- repos/example/SGP/trunk/src/main/java/pe/com/sgp/util/constants/Constants.java
+++ repos/example/SGP/trunk/src/main/java/pe/com/sgp/util/constants/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 
 	public static final String ERROR_ARTICULO_INVALID = "error.articulo.invalid";
 	public static final String ERROR_ARTICULO_ID_INVALID = "error.articulo.id.invalid";
+	public static final String ERROR_CLIENTE_ID_INVALID = "error.cliente.id.invalid";
 	
 }
