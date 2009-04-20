4ddcacfe8bde5ee22cb01505d48f7c432d57b395
wroquem <wroquem@164a9a6e-b8db-11dd-b55b-2da8d274f894>
2009-04-20 17:48:05 +0000

    add the database script creation

---
+++ repos/example/spring-mvc-crud-webapp/trunk/src/main/resources/script_create_db_demo.sql
@@ -0,0 +1,30 @@
+/*
+SQLyog Community Edition- MySQL GUI v7.02 
+MySQL - 5.0.67-community-nt : Database - db_demo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_demo` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_demo`;
+
+/*Table structure for table `person` */
+
+DROP TABLE IF EXISTS `person`;
+
+CREATE TABLE `person` (
+  `id` bigint(10) unsigned NOT NULL auto_increment,
+  `firstName` varchar(45) NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
